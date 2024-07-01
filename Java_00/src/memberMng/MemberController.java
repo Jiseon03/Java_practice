fedf30f5a94f62029d2d067a6c0e15d5b2f84c4b
USER <USER@1.209.180.143>
2024-07-01 12:27:36 +0900

    <2024.07.01> 회원 관리 프로그램 작성

---
+++ Java_00/src/memberMng/MemberController.java
@@ -0,0 +1,12 @@
+package memberMng;
+
+public class MemberController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MemberService memberService = new MemberService();
+		
+		memberService.startProgram();
+	}
+
+}
